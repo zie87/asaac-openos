b7f5a3a6f45735731bf3e7e24112bd531d059237
nils_paluch <nils_paluch>
2007-05-24 14:15:59 +0000

    git-svn-id: https://svn.code.sf.net/p/asaac-openos/code/DeveloperBranches@16 1faf1f51-c62e-0410-9343-f9b3b0d0658f

---
+++ ASAAC IF/ASAAC_IMPL.h
@@ -0,0 +1,99 @@
+#ifndef _ASAAC_IMPL_H_
+#define _ASAAC_IMPL_H_
+#include <time.h>
+
+#define _ASAAC_Bool_defined
+typedef enum {
+    ASAAC_BOOL_FALSE,
+    ASAAC_BOOL_TRUE
+} ASAAC_Bool;
+
+#define _ASAAC_Address_defined
+typedef void* ASAAC_Address ;
+
+#define _ASAAC_CharAddress_defined
+typedef char* ASAAC_CharAddress ;
+
+#define _ASAAC_GsmConfigData_defined
+typedef struct {
+	unsigned long  filler ;
+} ASAAC_GsmConfigData;
+
+#define _ASAAC_DataRepresentation_defined
+typedef char* ASAAC_DataRepresentation;
+
+#define _ASAAC_InterfaceType_defined
+typedef struct {
+	unsigned long  filler ;
+} ASAAC_InterfaceType;
+
+#define _ASAAC_LoadInstructions_defined
+typedef struct {
+	unsigned long  filler ;
+} ASAAC_LoadInstructions;
+
+#define _ASAAC_SchedulingInfo_defined
+typedef struct {
+	int   priority ;
+	ASAAC_Bool is_vc_attached ;
+	int   MAX_NR_OF_TRIGGER_VC ;
+	int   act_nr_of_trigger_vc ;
+	ASAAC_Bool is_periodic ;
+	struct timespec  start_time ;
+	struct timespec  period ;
+} ASAAC_SchedulingInfo;
+
+
+typedef enum {
+    NETWORK_TYPE_TCP ,
+    NETWORK_TYPE_UDP
+} NetworkType;
+
+typedef struct {
+    long* buffer_length;
+    void* stream_buffer;
+} TcpIpConfiguration;
+
+
+typedef struct { ;
+} UdpConfiguration;
+
+
+typedef union {
+	TcpIpConfiguration  tcpip ;
+	UdpConfiguration  udp ;
+} TcNetworkSpecific ;
+
+
+#define _ASAAC_TC_ConfigurationData_defined
+typedef struct  {
+	unsigned long     local_vc_id ;  // local vc for GSM instance specifies connection to Network Agent
+	NetworkType       network_type ;
+	TcNetworkSpecific conf_data ;
+} ASAAC_TC_ConfigurationData;
+
+
+// Character Sequence type copied from ASAAC.h
+// it is required for the ASAAC_DataRepresenation
+#if !defined(_ASAAC_CharacterSequence_defined)
+#define _ASAAC_CharacterSequence_defined 1
+typedef struct ASAAC_CharacterSequence_type ASAAC_CharacterSequence;
+struct ASAAC_CharacterSequence_type {
+unsigned long size;
+char data[256];
+};
+#endif
+
+//This type has been redifined here to avoid 
+//implicit conversion from TimeInterval to Time or vice versa.
+#define _ASAAC_TimeInterval_defined
+typedef struct {
+	long sec;
+	long nsec;
+} ASAAC_TimeInterval;
+
+//SMS.31.10.06> Sequence is the inproper type: char* defined instead
+//#define _ASAAC_DataRepresentation_defined
+//typedef ASAAC_CharacterSequence ASAAC_DataRepresentation;
+
+#endif //_ASAAC_IMPL_H_
