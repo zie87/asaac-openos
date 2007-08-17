df2ad2f4d8608fa2625f50e43d3256ff0d5e69e8
nils_paluch <nils_paluch>
2007-08-17 11:51:10 +0000

    git-svn-id: https://svn.code.sf.net/p/asaac-openos/code/DeveloperBranches@169 1faf1f51-c62e-0410-9343-f9b3b0d0658f

--- OpenOSRelease/ReleaseMaker.sh
+++ OpenOSRelease/ReleaseMaker.sh
@@ -5,19 +5,24 @@ mkdir OpenOS
 
 cp ../ASAAC\ IF/ASAAC.h OpenOS;                if (test $? != 0) then exit; fi;
 cp ../ASAAC\ IF/ASAAC_IMPL.h OpenOS;           if (test $? != 0) then exit; fi;
+cp ../ASAAC\ IF/ASAAC_ENH.h OpenOS;            if (test $? != 0) then exit; fi;
 
 cp ../OpenOSLib/OpenOS.h OpenOS;               if (test $? != 0) then exit; fi;
 cp ../OpenOSLib/OpenOS.hh OpenOS;              if (test $? != 0) then exit; fi;
 
+cp ../MOS/Debug/libMOS.so OpenOS;              if (test $? != 0) then exit; fi;
 cp ../OpenOSLib/Debug/libOpenOS.so OpenOS;     if (test $? != 0) then exit; fi;
 cp ../OpenOSEntity/Debug/OpenOSEntity OpenOS;  if (test $? != 0) then exit; fi;
 
-cp ../NII/Debug/libNII.a OpenOS;               if (test $? != 0) then exit; fi;
+cp ../AsaacCLib/Debug/libAsaacC.a OpenOS;      if (test $? != 0) then exit; fi;
+cp ../NIILib/Debug/libNII.a OpenOS;            if (test $? != 0) then exit; fi;
 cp ../PCS/Debug/PCS OpenOS;                    if (test $? != 0) then exit; fi;
 
 cp Delivery/* OpenOS;                          if (test $? != 0) then exit; fi;
 
+strip OpenOS/libMOS.so
 strip OpenOS/libOpenOS.so
+strip OpenOS/libAsaacC.a
 strip OpenOS/libNII.a
 strip OpenOS/OpenOSEntity
 strip OpenOS/PCS
@@ -25,6 +30,6 @@ strip OpenOS/PCS
 tar -cvzf Release.tgz OpenOS
 rm -rf OpenOS
 
-ftp -u ftp://upload.sourceforge.net/incoming/OpenOS-Beta1.tar.gz Release.tgz
+ftp -u ftp://upload.sourceforge.net/incoming/OpenOS-Beta2.tar.gz Release.tgz
 
 rm Release.tgz
