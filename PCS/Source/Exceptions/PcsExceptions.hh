24c38397b6049575641e82f1b1d9848039836dc6
nils_paluch <nils_paluch>
2008-02-19 16:53:10 +0000

    Removed file/folder
    git-svn-id: https://svn.code.sf.net/p/asaac-openos/code/DeveloperBranches@286 1faf1f51-c62e-0410-9343-f9b3b0d0658f

--- PCS/Source/Exceptions/PcsExceptions.hh
+++
@@ -1,7 +0,0 @@
-#ifndef PCSEXCEPTIONS_HH_
-#define PCSEXCEPTIONS_HH_
-
-#include "PcsException.hh"
-#include "PcsMemoryOverflowException.hh"
-
-#endif /*PCSEXCEPTIONS_HH_*/
--- PCS/Source/PcsHIncludes.hh
+++
@@ -1,9 +0,0 @@
-#ifndef PCSINCLUDES_HH_
-#define PCSINCLUDES_HH_
-
-#include "ASAAC_ENH.h"
-#include "PcsConstants.hh"
-#include "MOSConstants.h"
-#include "OpenOSIncludes.hh"
-
-#endif /*PCSINCLUDES_HH_*/
