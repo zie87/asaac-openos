483321d26bdab51ba54565a3c9bddde97b980ea4
nils_paluch <nils_paluch>
2008-10-16 13:09:26 +0000

    git-svn-id: https://svn.code.sf.net/p/asaac-openos/code/Releases/1.0a@314 1faf1f51-c62e-0410-9343-f9b3b0d0658f

---
+++ MOSLib/Source/MOSIncludes.h
@@ -0,0 +1,14 @@
+#ifndef MOSINCLUDES_H_
+#define MOSINCLUDES_H_
+
+#include "ASAAC.h"
+#include "AsaacCLib.h"
+#include "NII.h"
+
+#include "MOSConstants.h"
+
+#include <string.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+#endif /*MOSINCLUDES_H_*/
