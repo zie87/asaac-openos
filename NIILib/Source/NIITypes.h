da213245ebb149cd2c18407b638c66427a6039db
nils_paluch <nils_paluch>
2008-10-16 13:07:11 +0000

    git-svn-id: https://svn.code.sf.net/p/asaac-openos/code/Releases/1.0a@313 1faf1f51-c62e-0410-9343-f9b3b0d0658f

---
+++ NIILib/Source/NIITypes.h
@@ -0,0 +1,77 @@
+/// @file NIITypes.h
+/// @brief Contains the declaration of types, enums, structs and defines for use in NII module
+
+// Created:   21.08.2004
+// Authors:   S.Stratbuecker, N. Paluch
+// Copyright (c)  ESG GmbH 2007.
+
+#ifndef NII_TYPES_H
+#define NII_TYPES_H
+
+#include "ASAAC.h"
+#include "NIIConstants.h"
+
+
+/////////////////////////////////////////////
+/// send or received Tc Data
+
+
+typedef struct
+{
+	//Probably a header for data handling must be defined
+} NetworkHeader;
+
+typedef struct
+{
+	ASAAC_PublicId					tc_id;
+} TcHeader;
+
+
+typedef char TcData[NII_MAX_SIZE_OF_BUFFER];
+
+typedef struct  
+{
+	//NetworkHeader					network_header;
+	TcHeader    	                tc_header;
+	TcData		                    data;
+} TcPacket;
+
+typedef struct  
+{
+	TcPacket    	                packet;
+	unsigned long                   data_length;
+} TcPacketData;
+
+
+/////////////////////////////////////////////
+/// Internally used struct to associate a Network with an interface
+
+typedef struct 
+{
+  ASAAC_PublicId                    port;                ///< Network Identifier
+  ASAAC_InterfaceConfigurationData  configuration_data;  ///< Additional configuration data
+
+  int                               fd;                  ///< Socket File Descriptor
+  char								is_streaming;
+  ASAAC_PublicId					tc_id_with_data;
+} Network;
+
+
+/////////////////////////////////////////////
+/// Internally used struct to associate a Transfer Connection with a Network
+
+typedef struct
+{
+  ASAAC_PublicId                    tc_id;        ///< TC Identifier
+  ASAAC_NetworkDescriptor           network_id;   ///< Reference to associated Network
+  ASAAC_TransferDirection           direction;    ///< Sender or Receiver TC
+  ASAAC_TransferType                type;         ///< Message or Streaming TC
+
+  ASAAC_Bool 						trigger_callback;
+  ASAAC_PublicId                    callback_id;  ///< Identifier of Callback
+  EventInfoData						event_info_data;
+  
+  ASAAC_PublicId                    buffer_id;
+} TransferConnection;
+
+#endif
