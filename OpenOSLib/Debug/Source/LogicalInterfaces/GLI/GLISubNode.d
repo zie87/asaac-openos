f125aa01f1844a75b7c3d263f5c2990be2898c0e
gangkofer <gangkofer>
2008-12-15 08:48:52 +0000

    Patch from Sebastian Stratbücker on VC to TC mapping has been incorporated
    git-svn-id: https://svn.code.sf.net/p/asaac-openos/code/DeveloperBranches@357 1faf1f51-c62e-0410-9343-f9b3b0d0658f

---
+++ OpenOSLib/Debug/Source/LogicalInterfaces/GLI/GLISubNode.d
@@ -0,0 +1,171 @@
+Source/LogicalInterfaces/GLI/GLISubNode.d \
+  Source/LogicalInterfaces/GLI/GLISubNode.o:  \
+ ../Source/LogicalInterfaces/GLI/GLISubNode.cc \
+  ../Source/LogicalInterfaces/GLI/GLISubNode.hh \
+  ../Source/LogicalInterfaces/GLI/GLINode.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/OAL/oal.h \
+  /home/gam/IDEFIX/AsaacInterface/ASAAC.h \
+  /home/gam/IDEFIX/AsaacInterface/ASAAC_IMPL.h \
+  /home/gam/IDEFIX/OpenOSLib/Source/OpenOSConstants.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/OpenOSTypes.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Debug/Debug.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/CharacterSequence.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/TimeInterval.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/TimeStamp.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/TimeInterval.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/TimeStamp.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Common/Aid.h \
+  /home/gam/IDEFIX/AsaacInterface/ASAAC.h \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/Exceptions.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/ASAAC_Exception.hh \
+  /home/gam/IDEFIX/AsaacInterface/ASAAC.h \
+  /home/gam/IDEFIX/OpenOSLib/Source/OAL/oal.h \
+  /home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/CharacterSequence.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/APOSClientException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/ApplicationException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/FatalException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/HWFailureException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/HWResourceException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/OSException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/ProcessorException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/ResourceException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/SMBPException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/SMOSException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/DoubleInitializationException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/TimeoutException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/UninitializedObjectException.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/OpenOSObject.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/Exceptions.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Common/Templates/Shared.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Exceptions/Exceptions.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Allocator/Allocator.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Allocator/SharedMemory.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Allocator/Allocator.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/IPC/SimpleCommandInterface.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Common/Templates/Shared.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/IPC/Semaphore.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/AbstractInterfaces/LockingObject.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/IPC/Event.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/IPC/NoLock.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Common/Algorithms/Primes.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh \
+  /home/gam/IDEFIX/OpenOSLib/Source/Common/Algorithms/DiffieHellman.hh
+
+../Source/LogicalInterfaces/GLI/GLISubNode.hh:
+
+../Source/LogicalInterfaces/GLI/GLINode.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OAL/oal.h:
+
+/home/gam/IDEFIX/AsaacInterface/ASAAC.h:
+
+/home/gam/IDEFIX/AsaacInterface/ASAAC_IMPL.h:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OpenOSConstants.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OpenOSTypes.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Debug/Debug.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/CharacterSequence.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/TimeInterval.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/TimeStamp.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/TimeInterval.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/TimeStamp.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Common/Aid.h:
+
+/home/gam/IDEFIX/AsaacInterface/ASAAC.h:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/Exceptions.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/ASAAC_Exception.hh:
+
+/home/gam/IDEFIX/AsaacInterface/ASAAC.h:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OAL/oal.h:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Common/Asaac/CharacterSequence.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/APOSClientException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/ApplicationException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/FatalException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/HWFailureException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/HWResourceException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/OSException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/ProcessorException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/ResourceException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/SMBPException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/SMOSException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/DoubleInitializationException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/TimeoutException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/UninitializedObjectException.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OpenOSObject.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/Exceptions.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Common/Templates/Shared.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Exceptions/Exceptions.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Allocator/Allocator.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Allocator/SharedMemory.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Allocator/Allocator.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/IPC/SimpleCommandInterface.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Common/Templates/Shared.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/IPC/Semaphore.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/AbstractInterfaces/LockingObject.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/IPC/Event.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/IPC/NoLock.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Common/Algorithms/Primes.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/OpenOSIncludes.hh:
+
+/home/gam/IDEFIX/OpenOSLib/Source/Common/Algorithms/DiffieHellman.hh:
